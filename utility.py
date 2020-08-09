5450b7d8abeb6c4b777f94064bd178e2741d68bc
puraminy <pouramini@gmail.com>
2020-08-09 14:31:37 +0430

    Adding utility.py

--- utility.py
+++ utility.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import getch as gh
+import curses as cur
 
 if os.name == 'nt':
     import msvcrt
@@ -11,6 +12,8 @@ if os.name == 'nt':
                     ("visible", ctypes.c_byte)]
 
 def hide_cursor():
+    cur.curs_set(0)
+    return
     if os.name == 'nt':
         ci = _CursorInfo()
         handle = ctypes.windll.kernel32.GetStdHandle(-11)
@@ -22,6 +25,8 @@ def hide_cursor():
         sys.stdout.flush()
 
 def show_cursor():
+    cur.curs_set(1)
+    return
     if os.name == 'nt':
         ci = _CursorInfo()
         handle = ctypes.windll.kernel32.GetStdHandle(-11)
@@ -32,20 +37,34 @@ def show_cursor():
         sys.stdout.write("\033[?25h")
         sys.stdout.flush()
 
-def print_there(x, y, text):
-     sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
-     sys.stdout.flush()
 
-def get_key():
-    first_char = gh.getch()
-    if first_char == '\x1b':
-        try:
-            ret = {'[2':'insert','[3':'delete', '[H':'home','[F':'end', '[5':'pgup','[6':'pgdown','[A': 'up', '[B': 'down', '[C': 'right', '[D': 'left'}[gh.getch() + gh.getch()]
-        except:
-            ret = 'NA'
-        return ret
+def print_there(x, y, text, stdscr = None, color=0):
+    if stdscr is not None:
+         stdscr.addstr(x, y, text, cur.color_pair(color))
+         stdscr.refresh()
     else:
-        return first_char
+         sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
+         sys.stdout.flush()
+def clear_screen(stdscr = None):
+    if stdscr is not None:
+        stdscr.clear()
+        stdscr.refresh()
+    else:
+        os.system('clear')
+
+def get_key(stdscr = None):
+    if stdscr is not None:
+        return stdscr.getch()
+    else:
+        first_char = gh.getch()
+        if first_char == '\x1b':
+            try:
+                ret = {'[2':'insert','[3':'delete', '[H':'home','[F':'end', '[5':'pgup','[6':'pgdown','[A': 'up', '[B': 'down', '[C': 'right', '[D': 'left'}[gh.getch() + gh.getch()]
+            except:
+                ret = 'NA'
+            return ret
+        else:
+            return first_char
 
 
 # -*- coding: utf-8 -*-
@@ -56,8 +75,9 @@ suffixes = "(Inc|Ltd|Jr|Sr|Co)"
 starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
 acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
 websites = "[.](com|net|org|io|gov)"
+digits = "([0-9]+)"
 
-def split_into_sentences_old(text):
+def rplit_into_sentences(text):
     try:
         import nltk
         try:
@@ -68,19 +88,25 @@ def split_into_sentences_old(text):
             sents = nltk.sent_tokenize(text)
             return sents
     except ImportError as e:
-        return split2(text) 
+        return rplit_into_sentences(text)
 
 def split_into_sentences(text):
     text = " " + text + "  "
     text = text.replace("\n"," ")
     text = re.sub(prefixes,"\\1<prd>",text)
     text = re.sub(websites,"<prd>\\1",text)
-    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = text.replace("et al.","et al<prd>")
+    text = text.replace("e.g.","e<prd>g<prd>")
+    text = text.replace("i.e.","i<prd>e<prd>")
     text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
     text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
     text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
     text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
     text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" (\d+)[.](\d+) "," \\1<prd>\\2 ",text)
+    text = text.replace("...","<prd><prd><prd>")
+    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
     text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
     text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
     if "”" in text: text = text.replace(".”","”.")
@@ -93,5 +119,5 @@ def split_into_sentences(text):
     text = text.replace("<prd>",".")
     sentences = text.split("<stop>")
     sentences = sentences[:-1]
-    sentences = [s.strip() for s in sentences]
+    sentences = [s.strip() for s in sentences if len(s) > 10]
     return sentences
