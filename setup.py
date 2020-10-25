c51e97f6eef43218937d5a7363e70fb2259fb323
puraminy <pouramini@gmail.com>
2020-10-25 14:53:22 +0330

    Adding setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 import setuptools
+import os, sys
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+reqs = [i.strip() for i in open("requirements.txt").readlines()]
+reqs = ["requests", "appdirs"]
+if os.name == "nt" or sys.platform.lower().startswith("win"):
+     reqs += ['windows-curses']
 setuptools.setup(
     name="nodreader", # Replace with your own username
-    version="0.0.1",
+    version="0.1.2",
     author="A Pouramini",
     author_email="pouramini@gmail.com",
     description="Nodreader, a text-based article reader and manager",
@@ -17,14 +21,12 @@ setuptools.setup(
     entry_points={
         'console_scripts': [
             'nodreader = nodreader.nodreader:main',
-            'nr_resize = nodreader.resize:main',
-            'nr_resize2 = nodreader.resize2:main',
         ],
     },
-    install_requires=REQUIREMENTS,
+    install_requires=reqs,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
